42a3d535190d10fceed0d4555bba100682a6c6b8
hugosenari <hugosenari@gmail.com>
2022-03-12 04:02:39 -0300

    chore: re index tweens

---
+++ master/flake.nix
@@ -0,0 +1,26 @@
+{
+  description = ''Basic tweening library for Nim'';
+
+  inputs.flakeNimbleLib.owner = "riinr";
+  inputs.flakeNimbleLib.ref   = "master";
+  inputs.flakeNimbleLib.repo  = "nim-flakes-lib";
+  inputs.flakeNimbleLib.type  = "github";
+  inputs.flakeNimbleLib.inputs.nixpkgs.follows = "nixpkgs";
+  
+  inputs.src-tweens-master.flake = false;
+  inputs.src-tweens-master.owner = "RainbowAsteroids";
+  inputs.src-tweens-master.ref   = "refs/heads/master";
+  inputs.src-tweens-master.repo  = "tweens";
+  inputs.src-tweens-master.type  = "github";
+  
+  outputs = { self, nixpkgs, flakeNimbleLib, ...}@deps:
+  let 
+    lib  = flakeNimbleLib.lib;
+    args = ["self" "nixpkgs" "flakeNimbleLib" "src-tweens-master"];
+  in lib.mkRefOutput {
+    inherit self nixpkgs ;
+    src  = deps."src-tweens-master";
+    deps = builtins.removeAttrs deps args;
+    meta = builtins.fromJSON (builtins.readFile ./meta.json);
+  };
+}
