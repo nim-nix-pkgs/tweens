e1e616b2f0ff30288bdffea8f65facb5e1f9ca77
hugosenari <hugosenari@gmail.com>
2022-05-09 20:44:54 -0300

    chore: re index tweens

--- flake.nix
+++ flake.nix
@@ -7,14 +7,6 @@
   inputs.flakeNimbleLib.type  = "github";
   inputs.flakeNimbleLib.inputs.nixpkgs.follows = "nixpkgs";
   
-  inputs."tweens-master".dir   = "master";
-  inputs."tweens-master".owner = "nim-nix-pkgs";
-  inputs."tweens-master".ref   = "master";
-  inputs."tweens-master".repo  = "tweens";
-  inputs."tweens-master".type  = "github";
-  inputs."tweens-master".inputs.nixpkgs.follows = "nixpkgs";
-  inputs."tweens-master".inputs.flakeNimbleLib.follows = "flakeNimbleLib";
-  
   outputs = { self, nixpkgs, flakeNimbleLib, ...}@inputs:
   let 
     lib  = flakeNimbleLib.lib;
